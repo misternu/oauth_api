79a99fbce08080985f21b1f4182afdf9d6035771
Manu Meixell <misternu@gmail.com>
2022-05-01 18:33:01 -0500

    doorkeeper initial setup

--- app/controllers/api/resources_controller.rb
+++ app/controllers/api/resources_controller.rb
@@ -1,4 +1,6 @@
 class Api::ResourcesController < ActionController::API
+  before_action :doorkeeper_authorize!
+  
   def index
     render json: Resource.all
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all
+
+  has_many :oauth_applications,
+           class_name: "Doorkeeper::Application",
+           as: :owner
+
   has_many :resources
 end
